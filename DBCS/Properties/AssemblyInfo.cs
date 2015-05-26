c887c47cb379a8f06f7f4ffa0288c5e43255f541
cooclsee <cooclsee@outlook.com>
2015-05-26 16:38:17 +0800

    change DBCS to PCL

--- DBCS/Properties/AssemblyInfo.cs
+++ DBCS/Properties/AssemblyInfo.cs
@@ -1,35 +1,30 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// 有关程序集的常规信息通过以下特性集
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
 [assembly: AssemblyTitle("DBCS")]
-[assembly: AssemblyDescription("DBCS for Silverlight")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DBCS")]
-[assembly: AssemblyCopyright("Copyright © Aimeast 2011")]
+[assembly: AssemblyCopyright("版权所有(C)  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("zh-Hans")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9275c1a0-c6cc-4444-a22b-1495d7a61d86")]
-
-// Version information for an assembly consists of the following four values:
+// 程序集的版本信息由以下四个值组成: 
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      主版本
+//      次版本
+//      生成号
+//      修订号
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.3")]
-[assembly: AssemblyFileVersion("1.0.2.3")]
+// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+// 方法是按如下所示使用“*”: 
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
